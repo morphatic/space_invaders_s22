3c3b94adfe43ba40c7624924e5e9b8d53ec3023d
Morgan Benton <morgan.benton@gmail.com>
2022-04-18 09:55:38 -0400

    add main function

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 """
 The main program file that runs everything
 """
+import curses
+
+from curses import wrapper
+from game import Game
+
+
+def main(stdscr):
+    curses.curs_set(False)  # makes the cursor invisible (sets visibility to False)
+
+    stdscr.nodelay(
+        True
+    )  # immediately handle user input (don't wait for "return" to be pressed)
+    stdscr.clear()
+
+    Game(stdscr).run()
+
+
+if __name__ == "__main__":
+    wrapper(main)
