ab5dbc27cbddada463e06460d7fd2aec65081afe
Morgan Benton <morgan.benton@gmail.com>
2022-04-18 09:46:49 -0400

    add Laser class

--- laser.py
+++ laser.py
@@ -1,3 +1,26 @@
 """
 Laser
 """
+import curses
+
+
+class Laser:
+    def __init__(self, stdscr, x, y):
+        self.stdscr = stdscr
+        self.x = x
+        self.y = y
+        self.color = 7
+        self.width = 1
+
+    def tick(self, tick_number):
+        if self.y <= 0:
+            self.color = 8
+        else:
+            self.y -= 1
+
+        self.draw()
+
+    def draw(self):
+        self.stdscr.addstr(
+            self.y, self.x, " " * self.width, curses.color_pair(self.color)
+        )
